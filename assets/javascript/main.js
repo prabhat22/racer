9735f90d3eaaea2b4f12f86a7bbdb2a3c85ecfbf
prabhat22 <prabhatlokwani5@gmail.com>
2017-07-27 23:07:04 +0530

    Corosa racer
    racer webpage

---
+++ assets/javascript/main.js
@@ -0,0 +1,51 @@
+ $(document).ready(function(){
+        new WOW().init();
+       var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+   
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+     
+     $('.jq').LineProgressbar({
+
+  percentage: 90 ,
+         duration: 60000,
+  fillBackgroundColor: 'red',
+         width:'80',
+
+  backgroundColor: '#EEEEEE',
+
+  radius: '0px',
+
+  height: '10px',
+
+  width: '100%'
+
+});
+   
+     $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 60000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+
+      
+
+ 
+    });
